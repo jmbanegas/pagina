1757213d628d329e4cb7de65800a6bc09602bb65
johanna <jmbanegas@sudamericano.edu.ec>
2024-01-23 01:36:59 -0500

    codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContenidoComponent, EncabezadoComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
